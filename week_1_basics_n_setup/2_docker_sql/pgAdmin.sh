7078dfe5b75fd03cc83dca2362f1e99accc28e65
Jonathon Kobyluck <comet.cetera@live.com>
2023-11-08 11:11:49 -0700

    DEZ-15: create docker network; point to at container creation

--- week_1_basics_n_setup/2_docker_sql/pgAdmin.sh
+++ week_1_basics_n_setup/2_docker_sql/pgAdmin.sh
@@ -1,5 +1,13 @@
+#!/usr/bin/env bash
+
+# -e (env vars):  These are here to define "root" access
+# -p (port):      Publish a container's port(s) to the host, mapping as <HOSTPORT>:<CONTAINERPORT>;
+#                 map TCP port 80 in the container to port 8080 on the Docker host
+
 docker run -it \
   -e PGADMIN_DEFAULT_EMAIL="admin@admin.com" \
   -e PGADMIN_DEFAULT_PASSWORD="admin" \
   -p 8080:80 \
+  --network=pg-network \
+  --name pg-admin \
   dpage/pgadmin4
