6720c8867210f6667af498f48d84d9460dbf598d
Jonathon Kobyluck <comet.cetera@live.com>
2023-12-30 10:54:34 -0700

    DEZ-22:
    - define global engine/connection
    - create dedicated S3 connection fn
    - build custom dataframe -> redshift fn using COPY
    - hardcode table CREATE statement
    - store_and_fwd_flag col: None -> empty string
    - parameterize main
    - store CREATE statement in .sql file

---
+++ week_2_workflow_orchestration/create.sql
@@ -0,0 +1,21 @@
+CREATE TABLE ny_taxi.yellow_taxi_data (
+    vendorid                BIGINT,
+    tpep_pickup_datetime    TIMESTAMP,
+    tpep_dropoff_datetime   TIMESTAMP,
+    passenger_count         DOUBLE PRECISION,
+    trip_distance           DOUBLE PRECISION,
+    ratecodeid              DOUBLE PRECISION,
+    store_and_fwd_flag      VARCHAR(256),
+    pulocationid            BIGINT,
+    dolocationid            BIGINT,
+    payment_type            BIGINT,
+    fare_amount             DOUBLE PRECISION,
+    extra                   DOUBLE PRECISION,
+    mta_tax                 DOUBLE PRECISION,
+    tip_amount              DOUBLE PRECISION,
+    tolls_amount            DOUBLE PRECISION,
+    improvement_surcharge   DOUBLE PRECISION,
+    total_amount            DOUBLE PRECISION,
+    congestion_surcharge    DOUBLE PRECISION,
+    airport_fee             DOUBLE PRECISION
+);
